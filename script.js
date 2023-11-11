74395a67efce04abb21f54bfa6f1be7d5ab52304
rkkarthi07 <rkkarthick68@gmail.com>
2023-11-11 17:26:34 +0530

    Added all files

--- script.js
+++ script.js
@@ -236,6 +236,9 @@ async function merge(l, mid, r) {
         arr[k] = left[i]
         bl[k].style.backgroundColor = 'palevioletred'
         bl[k].style.height = `${left[i] * 3}px`;
+        if (width <= 960) {
+            bl[k].style.height = `${left[i] * 6}px`;
+        }
         bl[k].children[0].innerText = left[i]
         i++
         k++
@@ -246,6 +249,9 @@ async function merge(l, mid, r) {
         arr[k] = right[j]
         bl[k].style.backgroundColor = 'palevioletred'
         bl[k].style.height = `${right[j] * 3}px`;
+        if (width <= 960) {
+            bl[k].style.height = `${right[j] * 6}px`;
+        }
         bl[k].children[0].innerText = right[j]
         j++
         k++
